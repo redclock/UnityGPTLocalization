a4f5d69bd2ab922b429b2e9c535a78b9b00117e0
crimi <38652388+hikedev@users.noreply.github.com>
2024-07-06 10:58:11 +0700

    Update LocalizeGptWindow.cs
    Add model gpt-4o

--- Editor/LocalizeGptWindow/LocalizeGptWindow.cs
+++ Editor/LocalizeGptWindow/LocalizeGptWindow.cs
@@ -13,7 +13,8 @@ namespace RedGame.Framework.EditorTools
             "gpt-4-0125-preview",
             "gpt-4-1106-preview",
             "gpt-3.5-turbo-0125",
-            "gpt-3.5-turbo-1106"
+            "gpt-3.5-turbo-1106",
+            "gpt-4o"
         };
         
         private float _temperature;
